e2e4e2106b6e4c3f4bc05226d8c9638ffd705812
Amaal Ali <amaalalikhadija@gmail.com>
2023-02-08 20:08:45 -0500

    finished protecting from malicioous attacks

--- bootcamp_app/teachers.js
+++ bootcamp_app/teachers.js
@@ -6,14 +6,16 @@ const pool = new Pool({
   host: 'localhost',
   database: 'bootcampx'
 });
-
+const cohortName = process.argv[2];
+// Store all potentially malicious values in an array.
+const values = [cohortName];
 pool.query(`
 SELECT DISTINCT teachers.name as teacher, cohorts.name as cohort, COUNT(assistance_requests.*) AS total_assisstances
 FROM cohorts
 JOIN students ON cohort_id = cohorts.id
 JOIN assistance_requests ON student_id= students.id
 JOIN teachers ON teachers.id = teacher_id
-WHERE cohorts.name LIKE '${process.argv[2] || 'JUL02'}'
+WHERE cohorts.name LIKE #1
 
 GROUP BY teachers.name,cohorts.name
 ORDER BY teacher;
